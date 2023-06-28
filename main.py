731f26e8e507207d97eafe54693ae0ebf653267f
E-taku <tkymtk8270@gmail.com>
2023-06-28 14:17:26 +0900

    ✨ Separate files by number of amino acid residues

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import os
+import bisect
 
 def extractSeqs(ideal_data_path: str):
     aminoSeqs = []
@@ -9,6 +11,7 @@ def extractSeqs(ideal_data_path: str):
                 aminoSeqs.append(idx_seq)
     return aminoSeqs
 
+# remove all amino acids
 def writeSeqsToFile(writeFilePath: str, aminoSeqs: list):
     with open("./data/idealAminoSeqs.txt", "w") as f:
         for l in aminoSeqs:
@@ -16,7 +19,33 @@ def writeSeqsToFile(writeFilePath: str, aminoSeqs: list):
             aminoSeq = l[0][1]
             f.write(acc_number + " " + aminoSeq + "\n")
 
+def writeSeqsCutByLengthToFile(residuesNum: int):
+    allAminoSeqsInfo = []
+    maxSeqLength = 0
+    with open("./data/idealAminoSeqs.txt", "r") as f:
+        for s_line in f:
+            acc, aminoSeq = s_line.rstrip().split()
+            allAminoSeqsInfo.append([acc, aminoSeq])
+            maxSeqLength = max(maxSeqLength, len(aminoSeq))
+
+    cutLengthList = [i * residuesNum for i in range((maxSeqLength // residuesNum)+2)]
+    for acc, seq in allAminoSeqsInfo:
+        seqLenfth = len(seq)
+        idx = bisect.bisect_left(cutLengthList, seqLenfth)
+        dirname = "./data/every{}/".format(residuesNum)
+        if not(os.path.exists(dirname)):
+            os.mkdir(dirname)
+        # bisect_leftなので左準拠
+        dirname = "./data/every{}/{}~{}/".format(residuesNum, cutLengthList[idx-1], cutLengthList[idx])
+        if not(os.path.exists(dirname)):
+            os.mkdir(dirname)
+        filePath = "{}{}~{}.txt".format(dirname, cutLengthList[idx-1], cutLengthList[idx])
+        with open(filePath, mode='a') as f:
+            f.write(acc + " " + seq + "\n")
+    return allAminoSeqsInfo
+
 ideal_data_path = './data/ideal-v3.ttl'
 writeFilePath = './data/idealAminoSeqs.txt'
 aminoSeqs = extractSeqs(ideal_data_path)
 writeSeqsToFile(ideal_data_path, aminoSeqs)
+writeSeqsCutByLengthToFile(100)
